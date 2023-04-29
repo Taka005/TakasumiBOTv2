be186540e860e8f36422b516d635d17d315b5805
Taka005 <01taka005@gmail.com>
2023-04-29 23:18:25 +0900

    chnage:微調整と言語ファイル、処理追加

---
+++ module/lib/lang.js
@@ -0,0 +1,12 @@
+module.exports = (guildId,text)=>{
+  const db = require("./db");
+  const ja = require("../../lang/ja.json");
+  const en = require("../../lang/en.json");
+
+  const lang = await db(`SELECT * FROM lang WHERE id = ${guildId} LIMIT 1;`);
+  if(lang[0]){
+    return en[text];
+  }else{
+    return ja[text];
+  }
+}
