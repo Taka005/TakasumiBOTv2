c5f582bdb513a22e75e7da19af00ae64dc6ea980
Taka005 <01taka005@gmail.com>
2023-04-30 10:29:39 +0900

    change:言語ファイル・処理調整

--- module/lib/lang.js
+++ module/lib/lang.js
@@ -1,12 +1,13 @@
-module.exports = async(guildId,text)=>{
+module.exports = async(guildId,text,format)=>{
+  const util = require("util");
   const db = require("./db");
   const ja = require("../../lang/ja.json");
   const en = require("../../lang/en.json");
 
   const lang = await db(`SELECT * FROM lang WHERE id = ${guildId} LIMIT 1;`);
   if(lang[0]){
-    return en[text];
+    return util.format(en[text],format);
   }else{
-    return ja[text];
+    return util.format(ja[text],format);
   }
 }
