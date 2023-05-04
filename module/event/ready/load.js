16aa0b2f1af8645b1297458ab6b3659ddc79aa2b
Taka005 <01taka005@gmail.com>
2023-05-04 03:26:01 +0000

    fix:envを使用するように

--- module/event/ready/load.js
+++ module/event/ready/load.js
@@ -1,8 +1,8 @@
 module.exports = async(client)=>{
   const cron = require("node-cron");
   const os = require("os");
+  require("dotenv");
   const db = require("../../lib/db");
-  const config = require("../../../config.json");
   cron.schedule("0 * * * * *",async()=>{
     const log = await db("SELECT * FROM log");
 
@@ -12,7 +12,7 @@ module.exports = async(client)=>{
     }
     const user = client.guilds.cache.map((g)=>g.memberCount).reduce((a,c)=>a+c);
     const guild = client.guilds.cache.size;
-    const count = await db(`SELECT * FROM count WHERE id = ${config.id} LIMIT 1;`);
+    const count = await db(`SELECT * FROM count WHERE id = ${process.env.ID} LIMIT 1;`);
     const cpu = (await new Promise((resolve)=>
       require("os-utils").cpuUsage(resolve)
     )*100).toFixed(2);
@@ -23,8 +23,7 @@ module.exports = async(client)=>{
 
       });
     }
-    console.log("a")
     await db(`INSERT INTO log (time, ping, user, guild, message, command, cpu, ram) VALUES(NOW(),"${ping}","${user}","${guild}","${count[0].message}","${count[0].command}","${cpu}","${ram}");`);
-    await db(`INSERT INTO count (id, message, command) VALUES("${config.id}","0","0") ON DUPLICATE KEY UPDATE id = VALUES (id),message = VALUES (message), command = VALUES (command);`);
+    await db(`INSERT INTO count (id, message, command) VALUES("${process.env.ID}","0","0") ON DUPLICATE KEY UPDATE id = VALUES (id), message = VALUES (message), command = VALUES (command);`);
   });
 }
--- module/lib/count.js
+++ module/lib/count.js
@@ -1,12 +1,12 @@
+require("dotenv");
 const db = require("./db");
-const config = require("../../config.json");
 module.exports = {
   "message":async()=>{
-    const data = await db(`SELECT * FROM count WHERE id = ${config.id} LIMIT 1;`);
-    await db(`UPDATE count SET message=${Number(data[0].message)+1} WHERE id=${config.id};`);
+    const data = await db(`SELECT * FROM count WHERE id = ${process.env.ID} LIMIT 1;`);
+    await db(`UPDATE count SET message=${Number(data[0].message)+1} WHERE id=${process.env.ID};`);
   },
   "command":async()=>{
-    const data = await db(`SELECT * FROM count WHERE id = ${config.id} LIMIT 1;`);
-    await db(`UPDATE count SET command=${Number(data[0].command)+1} WHERE id=${config.id};`);
+    const data = await db(`SELECT * FROM count WHERE id = ${process.env.ID} LIMIT 1;`);
+    await db(`UPDATE count SET command=${Number(data[0].command)+1} WHERE id=${process.env.ID};`);
   }
 }
