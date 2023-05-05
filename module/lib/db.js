30c02fe3ffc04b4b4de61dfcdde20d15ce4ea57a
Taka005 <01taka005@gmail.com>
2023-05-05 06:21:17 +0000

    change:poolを使用するように

--- module/lib/db.js
+++ module/lib/db.js
@@ -1,21 +1,23 @@
 const mysql = require("mysql");
+const util = require("util");
 require("dotenv").config();
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "public.bfv4d.tky1.mdbs.jp",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "bfv4d_",
-  multipleStatements: true
-});  
+  database: "bfv4d_"
+});
+pool.getConnection = util.promisify(pool.getConnection);
 
 module.exports = async(query)=>{
-  const util = require("util");
+  const connection = await pool.getConnection();
   connection.query = util.promisify(connection.query);
-
   try{
     return await connection.query(query);
   }catch(error){
     console.log(`\x1b[31mDB: ${error}\x1b[39m`);
     return [];
+  }finally{
+    connection.release();
   }
 }
