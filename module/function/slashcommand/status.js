5fcafbce08660d01c68df0277fb2c3b29de8fbbd
Taka005 <01taka005@gmail.com>
2023-05-08 22:48:15 +0900

    fix:統計情報を修正

--- module/function/slashcommand/status.js
+++ module/function/slashcommand/status.js
@@ -27,13 +27,11 @@ module.exports = async(interaction)=>{
     const message = await db(`SELECT SUM(message) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`);
     const command = await db(`SELECT SUM(command) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`);
 
-    const messageSign = (await db(`SELECT SUM(message) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`))[0].total - (await db(`SELECT SUM(message) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1}";`))[0].total;
-    const commandSign = (await db(`SELECT SUM(command) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`))[0].total - (await db(`SELECT SUM(command) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1}";`))[0].total;
+    const messageSign = (await db(`SELECT SUM(message) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`))[0].total - (await db(`SELECT SUM(CASE WHEN time BETWEEN "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1} 00:00:00" AND "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1} 23:59:59" THEN message ELSE 0 END) AS total FROM log;`))[0].total;
+    const commandSign = (await db(`SELECT SUM(command) as total FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}";`))[0].total - (await db(`SELECT SUM(CASE WHEN time BETWEEN "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1} 00:00:00" AND "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1} 23:59:59" THEN command ELSE 0 END) AS total FROM log`))[0].total;
 
     const guild = interaction.client.guilds.cache.size - (await db(`SELECT guild FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}" LIMIT 1;`))[0].guild;
     const user = interaction.client.guilds.cache.map((g)=>g.memberCount).reduce((a,c)=>a+c) - (await db(`SELECT user FROM log WHERE DATE(time) = "${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}" LIMIT 1;`))[0].user;
-
-    const chat = global.length/interaction.client.guilds.cache.size*100;
 
     await interaction.editReply({
       embeds:[{
@@ -47,7 +45,7 @@ module.exports = async(interaction)=>{
           },
           {
             name: "Discord",
-            value: `Ping: ${interaction.client.ws.ping}㍉秒\nGC登録数: ${global.length} / ${interaction.client.guilds.cache.size} (${Math.round(chat)}%)\nひろゆき登録数: ${hiroyuki.length}\nTakasumiBOT Account: ${account.length}人\nServer Uptime: ${Math.round(os.uptime() / 60)}分(BOT: ${Math.round(process.uptime() / 60)}分)`
+            value: `Ping: ${interaction.client.ws.ping}㍉秒\nGC登録数: ${global.length} / ${interaction.client.guilds.cache.size} (${Math.round(global.length/interaction.client.guilds.cache.size*100)}%)\nひろゆき登録数: ${hiroyuki.length}\nTakasumiBOT Account: ${account.length}人\nServer Uptime: ${Math.round(os.uptime() / 60)}分(BOT: ${Math.round(process.uptime() / 60)}分)`
           },
           {
             name: "統計データ",
