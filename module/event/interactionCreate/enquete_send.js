e706001bcfe1e3c16e65df17003a094d20992748
Taka005 <01taka005@gmail.com>
2023-06-24 00:11:34 +0900

    fix:少し修正

--- module/event/interactionCreate/enquete_send.js
+++ module/event/interactionCreate/enquete_send.js
@@ -31,7 +31,7 @@ module.exports = async(interaction)=>{
           .addComponents(
             new ButtonBuilder()
               .setLabel("回答する")
-              .serCustomId("enquete")
+              .setCustomId("enquete")
               .setStyle(ButtonStyle.Secondary))
       ]
     })
