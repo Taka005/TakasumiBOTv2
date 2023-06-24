d135da5b73c7eb879534cce9dfa40184ad541831
Taka005 <01taka005@gmail.com>
2023-06-24 13:22:39 +0900

    feat:activity

---
+++ module/function/slashcommand/activity.js
@@ -0,0 +1,34 @@
+module.exports = async(interaction)=>{
+  const { Colors } = require("discord.js");
+  if(!interaction.isChatInputCommand()) return;
+  if(interaction.commandName === "activity"){
+
+    if(!interaction.member.presence.activities.length) return await interaction.reply({
+      embeds:[{
+        color: Colors.Red,
+        author:{
+          name: "表示出来ませんでした",
+          icon_url: "https://cdn.taka.ml/images/system/error.png"
+        },
+        description: "アクティビティがありません"
+      }],
+      ephemeral: true
+    });
+
+
+    try{
+      const members = (await interaction.guild.members.fetch())
+        .filter(member=>{
+          return member.presence.activities.filter(memberActivitiy=>{
+            return interaction.member.presence.activities.filter(activitiy=>activitiy.name === memberActivitiy.name);
+          });
+        });
+
+      await interaction.reply({
+
+      });
+    }catch{
+
+    }
+  }
+}
