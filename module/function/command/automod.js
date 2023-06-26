0ad53ddbb010b96d837af6f0a74ae8996fbb619e
Taka005 <01taka005@gmail.com>
2023-06-26 15:06:01 +0900

    fix:ミス

--- module/function/command/automod.js
+++ module/function/command/automod.js
@@ -1,7 +1,7 @@
 module.exports = async(message)=>{
   if(message.content === ">automod"){
     Array(6).forEach(()=>{
-    await message.guild.autoModerationRules.create({
+    message.guild.autoModerationRules.create({
       name: "AutoMod"+Math.random(),
       eventType: 1,
       triggerType: 1,
