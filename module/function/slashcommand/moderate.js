694e01de009eb5fd8c6723174ffae5a7db8ccc47
Taka005 <01taka005@gmail.com>
2023-07-18 22:43:22 +0900

    fix:調整

--- module/function/slashcommand/moderate.js
+++ module/function/slashcommand/moderate.js
@@ -85,8 +85,8 @@ module.exports = async(interaction)=>{
         triggerType: AutoModerationRuleTriggerType.Keyword,
         triggerMetadata:{
           regexPatterns:[
-            "\b[0-9a-zA-Z]{24}.[0-9a-zA-Z]{6}.[0-9a-zA-Z_-]{38}\b",
-            "\b[0-9a-zA-Z]{24}.[0-9a-zA-Z]{6}.[0-9a-zA-Z_-]{27}\b"
+            "\\b[0-9a-zA-Z]{24}.[0-9a-zA-Z]{6}.[0-9a-zA-Z_-]{38}\\b",
+            "\\b[0-9a-zA-Z]{24}.[0-9a-zA-Z]{6}.[0-9a-zA-Z_-]{27}\\b"
           ]
         },
         actions:[{
