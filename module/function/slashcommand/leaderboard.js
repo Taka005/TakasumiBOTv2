d3e2873c0e426e5fb89ecabca512434bfe573683
Taka005 <01taka005@gmail.com>
2023-07-26 08:58:38 +0900

    fix:完成

--- module/function/slashcommand/leaderboard.js
+++ module/function/slashcommand/leaderboard.js
@@ -7,8 +7,8 @@ module.exports = async(interaction)=>{
 
     const data = (await db("SELECT id, amount FROM money ORDER BY amount DESC LIMIT 10;"))
       .sort((m1,m2)=>m2.amount - m1.amount);
-console.log(data)
-    const rank = await Promise.all(data.map(async(data,i)=>`${i+1}位 ${(await fetchUser(data.id))?.tag||"不明"} ${data.amount}円`));
+
+    const rank = await Promise.all(data.map(async(data,i)=>`${i+1}位 ${(await fetchUser(interaction.client,data.id))?.tag||"不明"} ${data.amount}円`));
 
     await interaction.reply({
       embeds:[{
