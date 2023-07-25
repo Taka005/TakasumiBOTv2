4f1521f6dc60161e1e6f9c80990997313a3b777c
Taka005 <01taka005@gmail.com>
2023-07-26 08:57:37 +0900

    test:2

--- module/function/slashcommand/leaderboard.js
+++ module/function/slashcommand/leaderboard.js
@@ -6,9 +6,9 @@ module.exports = async(interaction)=>{
   if(interaction.commandName === "leaderboard"){
 
     const data = (await db("SELECT id, amount FROM money ORDER BY amount DESC LIMIT 10;"))
-      .sort((m1,m2)=>m2.mount - m1.mount);
+      .sort((m1,m2)=>m2.amount - m1.amount);
 console.log(data)
-    const rank = await Promise.all(data.map(async(data,i)=>`${i+1}位 ${(await fetchUser(data.id))?.tag||"不明"} ${data.mount}円`));
+    const rank = await Promise.all(data.map(async(data,i)=>`${i+1}位 ${(await fetchUser(data.id))?.tag||"不明"} ${data.amount}円`));
 
     await interaction.reply({
       embeds:[{
