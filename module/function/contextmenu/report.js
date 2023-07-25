c95fe6573a8ba52042e7dbd8eb317155813c7fed
Taka005 <01taka005@gmail.com>
2023-07-25 23:15:47 +0900

    change:直近のメッセージも送信するように

--- module/function/contextmenu/report.js
+++ module/function/contextmenu/report.js
@@ -1,10 +1,32 @@
 module.exports = async(interaction)=>{
-  const { ButtonBuilder, ActionRowBuilder, ButtonStyle, Colors } = require("discord.js");
+  const { AttachmentBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, Colors } = require("discord.js");
   const config = require("../../../config.json");
+  const fetchMessage = require("../../lib/fetchMessage");
   if(!interaction.isContextMenuCommand()) return;
   if(interaction.commandName === "Report Message"){
     const message = interaction.options.getMessage("message");
-      
+
+    if(
+      !interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionFlagsBits.ReadMessageHistory)||
+      !interaction.guild.members.me.permissionsIn(interaction.channel).has(PermissionFlagsBits.ViewChannel)
+    ) return await interaction.reply({
+      embeds:[{
+        color: Colors.Red,
+        author:{
+          name: "BOTに権限がありません",
+          icon_url: "https://cdn.taka.cf/images/system/error.png"
+        },
+        description: "このコマンドはBOTに以下の権限が必要です",
+        fields:[
+          {
+            name: "必要な権限",
+            value: "```チャンネルの閲覧\nメッセージ履歴の閲覧```"
+          }
+        ]
+      }],
+      ephemeral: true
+    });
+
     if(!message.content) return await interaction.reply({
       embeds:[{
         author:{
@@ -17,6 +39,19 @@ module.exports = async(interaction)=>{
       ephemeral: true
     });
 
+    const messages = await fetchMessage(message.channel,{limit:"10"});
+    if(!messages) return await interaction.reply({
+      embeds:[{
+        author:{
+          name: "報告できませんでした",
+          icon_url: "https://cdn.taka.cf/images/system/error.png"
+        },
+        color: Colors.Red,
+        description: "BOTの権限が不足しています"
+      }],
+      ephemeral: true
+    });
+
     await interaction.client.channels.cache.get(config.report).send({
       embeds:[{
         color: Colors.Green,
@@ -24,13 +59,17 @@ module.exports = async(interaction)=>{
           name: "メッセージレポート",
           icon_url: "https://cdn.taka.cf/images/system/success.png",
         },
-        description: `**${message.author.tag}(${message.author.id})**\n${message.content}`,
         footer:{
           text: `${interaction.user.tag}(${interaction.user.id})`,
           icon_url: interaction.user.avatarURL()||interaction.user.defaultAvatarURL,
         },
         timestamp: new Date()
-      }]
+      }],
+      files:[
+        new AttachmentBuilder()
+          .setFile(new Buffer.from(messages.map(message=>`${message.author.tag}(${message.author.id})\n${message.content}\n`))) 
+          .setName(`REPORT_${interaction.user.id}.txt`)
+      ]
     })
       .then(async()=>{
         await interaction.reply({ 
