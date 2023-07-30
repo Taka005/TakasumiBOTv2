c2eb153799cb85640949a48d559fb436db737363
Taka005 <01taka005@gmail.com>
2023-07-30 17:22:47 +0900

    fix:調整

--- module/function/slashcommand/list.js
+++ module/function/slashcommand/list.js
@@ -6,14 +6,13 @@ module.exports = async(interaction)=>{
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "list"){
 
-    const servers = (await db(`SELECT * FROM server;`))
+    const servers = Promise.all((await db(`SELECT * FROM server;`))
       .sort((s1,s2)=>new Date(s1.time) - new Date(s1.time))
       .map(async(server)=>({
         guild: await fetchGuild(interaction.client,server.id),
         url: `https://discord.gg/${server.code}`,
         time: time(new Date()-new Date(server.time))
-      }));
-    console.log(servers)
+      })));
 
     servers.length = 10;
 
