83266557bf699453fe7119f0d28f29890959c209
Taka005 <01taka005@gmail.com>
2023-07-30 16:45:30 +0900

    fix:ミス

--- module/function/slashcommand/up.js
+++ module/function/slashcommand/up.js
@@ -85,7 +85,7 @@ module.exports = async(interaction)=>{
       ephemeral: true
     });
 
-    await db(`UPDATE server SET time = "NOW()" WHERE id = ${interaction.guild.id}`);
+    await db(`UPDATE server SET time = NOW() WHERE id = ${interaction.guild.id}`);
     await interaction.reply({
       embeds:[{
         color: Colors.Green,
