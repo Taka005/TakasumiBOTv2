a3f1c5f75b0f7e8f9d50e93e36e46c4e4aa2dc10
Taka005 <01taka005@gmail.com>
2023-07-31 22:26:23 +0900

    change:戻す

--- module/function/slashcommand/list.js
+++ module/function/slashcommand/list.js
@@ -5,8 +5,14 @@ module.exports = async(interaction)=>{
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "list"){
 
-    const servers = (await db(`SELECT * FROM server;`))
-      .sort((s1,s2)=>new Date(s2.time) - new Date(s1.time));
+    const servers = await Promise.all((await db(`SELECT * FROM server;`))
+      .sort((s1,s2)=>new Date(s2.time) - new Date(s1.time))
+      .map(async(server)=>({
+        guild: await fetchGuild(interaction.client,server.id),
+        url: `https://discord.gg/${server.code}`,
+        text: server.text,
+        time: time(new Date()-new Date(server.time))
+      })));
 
     servers.length = 5;
 
@@ -17,7 +23,7 @@ module.exports = async(interaction)=>{
           name: "サーバー一覧",
           icon_url: "https://cdn.taka.cf/images/system/success.png"
         },
-        description: servers.map(server=>`**[${server.name}](https://discord.gg/${server.code})(${server.members}人)** - ${time(new Date()-new Date(server.time))}前\n${server.text}`).join("\n\n")
+        description: servers.map(server=>`**[${server.guild.name}](${url})(${server.guild.memberCount}人)** - ${server.time}前\n${server.text}`).join("\n\n")
       }]
     });
   }
