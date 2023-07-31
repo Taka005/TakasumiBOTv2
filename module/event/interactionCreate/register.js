ebae8a9130650ad7528fd1f60affb1073ab4b495
Taka005 <01taka005@gmail.com>
2023-07-31 18:46:27 +0900

    fix:調整

--- module/event/interactionCreate/register.js
+++ module/event/interactionCreate/register.js
@@ -11,7 +11,7 @@ module.exports = async(interaction)=>{
       "maxAge": 0
     })
       .then(async(invite)=>{
-        await db(`INSERT INTO server (id, name, members, owner, code, text, time) VALUES("${interaction.guild.id}","${interaction.guild.name}","${interaction.guild.memberCount}","${interaction.guild.ownerId}","${invite.code}","${text}",NOW());`);
+        await db(`INSERT INTO server (id, name, members, owner, code, text, time) VALUES("${interaction.guild.id}","${interaction.guild.name.replace(/[^\x00-\x7F]/g,"")}","${interaction.guild.memberCount}","${interaction.guild.ownerId}","${invite.code}","${text}",NOW());`);
 
         await interaction.reply({
           embeds:[{
