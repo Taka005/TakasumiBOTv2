d95c17a3d63c7e94abf2b0fc7e440030f9fd8d04
Taka005 <01taka005@gmail.com>
2023-08-09 08:21:51 +0900

    change:キャッシュを制御

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, Colors } = require("discord.js");
+const { Client, GatewayIntentBits, Options, Colors } = require("discord.js");
 require("dotenv").config();
 const config = require("./config.json"); 
 
@@ -9,7 +9,25 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildPresences
-  ]
+  ],
+  makeCache: Options.cacheWithLimits({
+    BaseGuildEmojiManager: 0,
+    GuildEmojiManager: 0,
+    GuildEmojiRoleManager: 0,
+    GuildForumThreadManager: 0,
+    GuildScheduledEventManager: 0,
+    GuildStickerManager: 0,
+    GuildTextThreadManager: 0,
+    ReactionManager: 0,
+    ReactionUserManager: 0,
+    MessageManager: 0,
+    StageInstanceManager: 0,
+    ThreadManager: 0,
+    ThreadMemberManager: 0,
+    AutoModerationRuleManager: 0,
+    VoiceStateManager: 0,
+    GuildInviteManager: 0
+  })
 });
 
 console.log("\x1b[32m*****************************\x1b[39m");
