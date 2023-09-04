214d8a82d77a92e183d6872a43b23174c8d3a379
Taka005 <01taka005@gmail.com>
2023-09-04 14:02:47 +0900

    change:色々変更

--- module/function/slashcommand/leaderboard.js
+++ module/function/slashcommand/leaderboard.js
@@ -10,7 +10,7 @@ module.exports = async(interaction)=>{
     const data = (await db("SELECT * FROM money;"))
       .sort((m1,m2)=>m2.amount - m1.amount);
 
-    data.length = 10;
+    data.length = 15;
 
     const rank = await Promise.all(data.map(async(data,i)=>`**${i+1}位** ${(await fetchUser(interaction.client,data.id))?.tag||"不明"} - ${data.amount}円`));
 
--- module/function/slashcommand/miq.js
+++ module/function/slashcommand/miq.js
@@ -4,11 +4,12 @@ module.exports = async(interaction)=>{
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "miq"){
     const type = interaction.options.getString("type");
+    const user = interaction.options.getUser("user");
     const text = interaction.options.getString("text");
   
     await interaction.deferReply();
     try{
-      const image = await fetch(`http://localhost:3000/?type=${type}&name=${interaction.user.username}&id=${interaction.user.id}&content=${text.replace("#","＃")}&icon=${interaction.user.avatarURL({extension:"png",size:1024})||interaction.user.defaultAvatarURL}`)
+      const image = await fetch(`http://localhost:3000/?type=${type}&name=${user.username}&id=${interaction.user.id}&content=${text.replace("#","＃")}&icon=${user.avatarURL({extension:"png",size:1024})||user.defaultAvatarURL}`)
         .then(res=>res.blob());
   
       await interaction.editReply({
