6c43b0a173cc03a10b34532ca2d6653e3300daa7
Taka005 <01taka005@gmail.com>
2023-09-16 09:49:10 +0900

    fix:修正

--- module/lib/fetchGuild.js
+++ module/lib/fetchGuild.js
@@ -1,12 +1,6 @@
 module.exports = async(client,guildId)=>{
   try{
-    if(client.shard){
-      return await client.shard.broadcastEval((c,id)=>c.guilds.fetch(id),{ 
-        context: guildId
-      }).find(res=>res)||null;
-    }else{
-      return await client.guilds.fetch(guildId);
-    }
+    return await client.guilds.fetch(guildId);
   }catch{
     return null;
   }
