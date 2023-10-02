c07e20aad0bb902d87761441f298c2a705b1fb96
Taka005 <01taka005@gmail.com>
2023-10-02 17:14:56 +0900

    fix:ミス

--- module/event/messageCreate/pin.js
+++ module/event/messageCreate/pin.js
@@ -31,8 +31,7 @@ module.exports = async(message)=>{
         }]
       });
       await db(`UPDATE pin SET message = ${after.id} WHERE channel = ${message.channel.id};`);
-    }catch(error){
-      message.channel.send(error.stack);
+    }catch{
       await db(`UPDATE pin SET count = ${Number(channel[0].count)-1} WHERE server = ${message.guild.id};`);
       await db(`DELETE FROM pin WHERE channel = ${message.channel.id} LIMIT 1;`);
     }
--- module/lib/wait.js
+++ module/lib/wait.js
@@ -1,7 +1,7 @@
 module.exports = class Wait{
   constructor(rate){
     this.time = {};
-    this.rate;
+    this.rate = rate;
   }
 
   count(id){
