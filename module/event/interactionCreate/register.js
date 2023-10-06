53e8929e083f11d1ccee77923eac0370f92e20ac
Taka005 <01taka005@gmail.com>
2023-10-06 23:53:32 +0900

    change:少し変更

--- module/event/interactionCreate/register.js
+++ module/event/interactionCreate/register.js
@@ -7,49 +7,49 @@ module.exports = async(interaction)=>{
     const text = interaction.fields.getTextInputValue("text")
       .replace(/\\n/g,"");
 
-    await interaction.channel.createInvite({
-      "unique": true,
-      "maxAge": 0
-    })
-      .then(async(invite)=>{
-        await db(`INSERT INTO server (id, name, count, owner, code, text, time) VALUES("${interaction.guild.id}","${escape(interaction.guild.name)}","${interaction.guild.memberCount}","${interaction.guild.ownerId}","${invite.code}","${escape(text)}",NOW());`);
-
-        await interaction.reply({
-          embeds:[{
-            color: Colors.Green,
-            author:{
-              name: "登録しました",
-              icon_url: "https://cdn.taka.cf/images/system/success.png"
-            },
-            description: "サーバー掲示板に公開しました"
-          }]
-        });
-      })
-      .catch(async(error)=>{
-        await interaction.reply({
-          embeds:[{
-            color: Colors.Red,
-            author:{
-              name: "登録できませんでした",
-              icon_url: "https://cdn.taka.cf/images/system/error.png"
-            },
-            fields:[
-              {
-                name: "エラーコード",
-                value: `\`\`\`${error}\`\`\``
-              }
-            ]
-          }],
-          components:[
-            new ActionRowBuilder()
-              .addComponents( 
-                new ButtonBuilder()
-                  .setLabel("サポートサーバー")
-                  .setURL("https://discord.gg/NEesRdGQwD")
-                  .setStyle(ButtonStyle.Link))
-          ],
-          ephemeral: true
-        });
+    try{
+      const invite = await interaction.channel.createInvite({
+        "unique": true,
+        "maxAge": 0
       });
+      
+      await db(`INSERT INTO server (id, name, count, owner, code, text, time) VALUES("${interaction.guild.id}","${escape(interaction.guild.name)}","${interaction.guild.memberCount}","${interaction.guild.ownerId}","${invite.code}","${escape(text)}",NOW());`);
+
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Green,
+          author:{
+            name: "登録しました",
+            icon_url: "https://cdn.taka.cf/images/system/success.png"
+          },
+          description: "サーバー掲示板に公開しました"
+        }]
+      });
+    }catch(error){
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Red,
+          author:{
+            name: "登録できませんでした",
+            icon_url: "https://cdn.taka.cf/images/system/error.png"
+          },
+          fields:[
+            {
+              name: "エラーコード",
+              value: `\`\`\`${error}\`\`\``
+            }
+          ]
+        }],
+        components:[
+          new ActionRowBuilder()
+            .addComponents( 
+              new ButtonBuilder()
+                .setLabel("サポートサーバー")
+                .setURL("https://discord.gg/NEesRdGQwD")
+                .setStyle(ButtonStyle.Link))
+        ],
+        ephemeral: true
+      });
+    }
   }
 }
--- module/function/contextmenu/pin.js
+++ module/function/contextmenu/pin.js
@@ -123,47 +123,21 @@ module.exports = async(interaction)=>{
         });
       }
     }else{
-      try{
-        await (await message.channel.messages.fetch(channel[0].message)).delete();
+      await (await message.channel.messages.fetch(channel[0].message)).delete()
+        .catch(()=>{});
 
-        await db(`UPDATE pin SET count = ${Number(server[0].count)-1} WHERE server = ${message.guild.id};`);
-        await db(`DELETE FROM pin WHERE channel = ${message.channel.id} LIMIT 1;`);
-  
-        await interaction.reply({
-          embeds:[{
-            color: Colors.Green,
-            author:{
-              name: "ピン留めを削除しました",
-              icon_url: "https://cdn.taka.cf/images/system/success.png"
-            }
-          }]
-        });
-      }catch(error){
-        await interaction.reply({
-          embeds:[{
-            color: Colors.Red,
-            author:{
-              name: "ピン留めを削除できませんでした",
-              icon_url: "https://cdn.taka.cf/images/system/error.png"
-            },
-            fields:[
-              {
-                name: "エラーコード",
-                value: `\`\`\`${error}\`\`\``
-              }
-            ]
-          }],
-          components:[
-            new ActionRowBuilder()
-              .addComponents( 
-                new ButtonBuilder()
-                  .setLabel("サポートサーバー")
-                  .setURL("https://discord.gg/NEesRdGQwD")
-                  .setStyle(ButtonStyle.Link))
-          ],
-          ephemeral: true
-        });
-      }
+      await db(`UPDATE pin SET count = ${Number(server[0].count)-1} WHERE server = ${message.guild.id};`);
+      await db(`DELETE FROM pin WHERE channel = ${message.channel.id} LIMIT 1;`);
+
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Green,
+          author:{
+            name: "ピン留めを削除しました",
+            icon_url: "https://cdn.taka.cf/images/system/success.png"
+          }
+        }]
+      });
     }
   }
 }
