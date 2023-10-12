5d3512a43b9d062ac52cce1f358c02babe7b4cc6
Taka005 <01taka005@gmail.com>
2023-10-12 17:04:59 +0900

    fix:調整

--- module/function/slashcommand/number.js
+++ module/function/slashcommand/number.js
@@ -1,5 +1,5 @@
 module.exports = async(interaction)=>{
-  const { ButtonBuilder, ActionRowBuilder, ButtonStyle, Colors } = require("discord.js");
+  const { Colors } = require("discord.js");
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "number"){
     const source = interaction.options.getString("source");
@@ -14,46 +14,21 @@ module.exports = async(interaction)=>{
           icon_url: "https://cdn.taka.cf/images/system/error.png"
         },
         description: "変換する値は数字を指定してください"
-      }]
+      }],
+      ephemeral: true
     });
   
-    try{
-      const data = parseInt(number,Number(source)).toString(Number(target));
-        
-      await interaction.reply({
-        embeds:[{
-          color: Colors.Green,
-          author:{
-            name: `${source}進数から${target}進数に変換しました`,
-            icon_url: "https://cdn.taka.cf/images/system/success.png"
-          },
-          description: `\`\`\`${data}\`\`\``
-        }]
-      });
-    }catch(error){
-      await interaction.reply({
-        embeds:[{
-          color: Colors.Red,
-          author:{
-            name: "変換できませんでした",
-            icon_url: "https://cdn.taka.cf/images/system/error.png"
-          },
-          fields:[
-            {
-              name: "エラーコード",
-              value: `\`\`\`${error}\`\`\``
-            }
-          ]
-        }],
-        components:[
-          new ActionRowBuilder()
-            .addComponents( 
-              new ButtonBuilder()
-                .setLabel("サポートサーバー")
-                .setURL("https://discord.gg/NEesRdGQwD")
-                .setStyle(ButtonStyle.Link))
-        ]
-      });
-    }
-    }
+    const data = parseInt(number,Number(source)).toString(Number(target));
+
+    await interaction.reply({
+      embeds:[{
+        color: Colors.Green,
+        author:{
+          name: `${source}進数から${target}進数に変換しました`,
+          icon_url: "https://cdn.taka.cf/images/system/success.png"
+        },
+        description: `\`\`\`${data}\`\`\``
+      }]
+    });
   }
+}
