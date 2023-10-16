ef67df2f8638248ec12478ce04622c8749d39ff9
Taka005 <01taka005@gmail.com>
2023-10-16 21:02:18 +0900

    change:変換追加

--- module/function/slashcommand/graph.js
+++ module/function/slashcommand/graph.js
@@ -3,15 +3,26 @@ module.exports = async(interaction)=>{
   const fetch = require("node-fetch");
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "graph"){
-    const formula = [
+    const formulas = [
       interaction.options.getString("formula_1"),
       interaction.options.getString("formula_2"),
       interaction.options.getString("formula_3")
-    ].filter(f=>f!==null).join(",");
+    ]
+      .filter(formula=>formula!==null)
+      .map(formula=>
+        formula
+          .replace((/[０-９]/gu),(str)=>String.fromCharCode(str.charCodeAt(0) - 65248))
+          .replace((/＋/gu),"+")
+          .replace((/ー/gu),"-")
+          .replace((/[x×]/gu),"*")
+          .replace((/÷/gu),"/")
+          .replace((/（/gu),"(")
+          .replace((/）/gu),")")
+      ).join(",");
   
     await interaction.deferReply();
     try{
-      const image = await fetch(`http://localhost:4000/?formula=${formula}`)
+      const image = await fetch(`http://localhost:4000/?formula=${formulas}`)
         .then(res=>res.blob());
   
         await interaction.editReply({
