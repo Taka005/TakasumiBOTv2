76af0785f65b11ef4addaeab3e7c18e5d895cc0e
Taka005 <01taka005@gmail.com>
2023-10-18 19:55:15 +0900

    change:招待更新を削除

--- module/function/slashcommand/up.js
+++ module/function/slashcommand/up.js
@@ -1,7 +1,6 @@
 module.exports = async(interaction)=>{
-  const { ButtonBuilder, ActionRowBuilder, ButtonStyle, PermissionFlagsBits, Colors } = require("discord.js");
+  const { PermissionFlagsBits, Colors } = require("discord.js");
   const db = require("../../lib/db");
-  const fetchInvite = require("../../lib/fetchInvite");
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "up"){
 
@@ -36,68 +35,31 @@ module.exports = async(interaction)=>{
       ephemeral: true
     });
 
-    try{
-      if(!await fetchInvite(interaction.guild,data[0].code)){
-        const invite = await interaction.channel.createInvite({
-          "unique": true,
-          "maxAge": 0
-        });
-  
-        await db(`UPDATE server SET code = "${invite.code}" WHERE id = ${interaction.guild.id}`);
-      }
+    if(new Date()-new Date(data[0].time)<3600000) return await interaction.reply({
+      embeds:[{
+        color: Colors.Red,
+        author:{
+          name: "UPできませんでした",
+          icon_url: "https://cdn.taka.cf/images/system/error.png"
+        },
+        description: `このサーバーを上げられるようになるまであと${Math.floor((3600000 - (new Date() - new Date(data[0].time)))/60000)}分です`
+      }],
+      ephemeral: true
+    });
 
-      if(new Date()-new Date(data[0].time)<3600000) return await interaction.reply({
-        embeds:[{
-          color: Colors.Red,
-          author:{
-            name: "UPできませんでした",
-            icon_url: "https://cdn.taka.cf/images/system/error.png"
-          },
-          description: `このサーバーを上げられるようになるまであと${Math.floor((3600000 - (new Date() - new Date(data[0].time)))/60000)}分です`
-        }],
-        ephemeral: true
-      });
-
-      await db(`UPDATE server SET name = "${interaction.guild.name}", count = "${interaction.guild.memberCount}", icon = "${interaction.guild.iconURL({extension:"png",size:1024})||"https://cdn.discordapp.com/embed/avatars/0.png"}", time = NOW() WHERE id = ${interaction.guild.id}`);
-      await interaction.reply({
-        embeds:[{
-          color: Colors.Green,
-          author:{
-            name: "UPしました!",
-            icon_url: "https://cdn.taka.cf/images/system/success.png"
-          },
-          image:{
-            url: "https://cdn.taka.cf/images/up.gif"
-          },
-          description: "表示順位が更新されました\n[サーバー掲示板](https://servers.taka.cf/)で確認してね!\n1時間後に通知します"
-        }]
-      });
-    }catch(error){
-      await db(`DELETE FROM server WHERE id = ${interaction.guild.id};`);
-      await interaction.reply({
-        embeds:[{
-          color: Colors.Red,
-          author:{
-            name: "UPできませんでした",
-            icon_url: "https://cdn.taka.cf/images/system/error.png"
-          },
-          fields:[
-            {
-              name: "エラーコード",
-              value: `\`\`\`${error}\`\`\``
-            }
-          ]
-        }],
-        components:[
-          new ActionRowBuilder()
-            .addComponents( 
-              new ButtonBuilder()
-                .setLabel("サポートサーバー")
-                .setURL("https://discord.gg/NEesRdGQwD")
-                .setStyle(ButtonStyle.Link))
-        ],
-        ephemeral: true
-      });
-    }
+    await db(`UPDATE server SET name = "${interaction.guild.name}", count = "${interaction.guild.memberCount}", icon = "${interaction.guild.iconURL({extension:"png",size:1024})||"https://cdn.discordapp.com/embed/avatars/0.png"}", time = NOW() WHERE id = ${interaction.guild.id}`);
+    await interaction.reply({
+      embeds:[{
+        color: Colors.Green,
+        author:{
+          name: "UPしました!",
+          icon_url: "https://cdn.taka.cf/images/system/success.png"
+        },
+        image:{
+          url: "https://cdn.taka.cf/images/up.gif"
+        },
+        description: "表示順位が更新されました\n[サーバー掲示板](https://servers.taka.cf/)で確認してね!\n1時間後に通知します"
+      }]
+    });
   }
 }
--- module/lib/fetchInvite.js
+++
@@ -1,7 +0,0 @@
-module.exports = async(guild,code)=>{
-  try{
-    return await guild.invites.fetch(code)
-  }catch{
-    return null;
-  }
-}
