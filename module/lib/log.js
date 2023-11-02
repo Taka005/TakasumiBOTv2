b56c582b1c552607b917ce70d3982e027182344a
Taka005 <takanosumika.sub@gmail.com>
2023-11-02 19:46:09 +0900

    style:調整

--- module/lib/log.js
+++ module/lib/log.js
@@ -3,27 +3,27 @@ const fs = require("fs");
 module.exports = {
   "info":(str)=>{
     const now = new Date();
-    console.log(`\x1b[32m[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [INFO] ${str}\x1b[39m`);
+    console.log(`\x1b[32m[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [INFO] ${str}\x1b[39m`);
 
-    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [INFO] ${str}\n`,"utf8");
+    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [INFO] ${str}\n`,"utf8");
   },
   "warn":(str)=>{
     const now = new Date();
-    console.warn(`\x1b[33m[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [WARN] ${str}\x1b[39m`);
+    console.warn(`\x1b[33m[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [WARN] ${str}\x1b[39m`);
 
-    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [WARN] ${str}\n`,"utf8");
+    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [WARN] ${str}\n`,"utf8");
   },
   "error":(str)=>{
     const now = new Date();
-    console.error(`\x1b[31m[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [ERROR] ${str}\x1b[39m`);
+    console.error(`\x1b[31m[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [ERROR] ${str}\x1b[39m`);
 
-    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [ERROR] ${str}\n`,"utf8");
+    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [ERROR] ${str}\n`,"utf8");
   },
   "debug":(str)=>{
     const now = new Date();
-    console.debug(`\x1b[34m[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [DEBUG] ${str}\x1b[39m`);
+    console.debug(`\x1b[34m[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [DEBUG] ${str}\x1b[39m`);
 
-    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [DEBUG] ${str}\n`,"utf8");
+    fs.appendFileSync("./tmp/app.log",`[${now.getMonth()+1}/${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}] [DEBUG] ${str}\n`,"utf8");
   },
   "reset":()=>{
     fs.writeFileSync("./tmp/app.log","","utf-8");
