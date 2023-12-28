75484a6823b6c66ca699a2becbaed5a10802ec50
Taka005 <01taka005@gmail.com>
2023-12-28 11:05:16 +0900

    fix:調整

--- module/function/slashcommand/server.js
+++ module/function/slashcommand/server.js
@@ -1,5 +1,5 @@
 module.exports = async(interaction)=>{
-  const { ButtonBuilder, ActionRowBuilder, ButtonStyle, ChannelType, Colors } = require("discord.js");
+  const { ButtonBuilder, ActionRowBuilder, ButtonStyle, ChannelType, Colors, ShardClientUtil } = require("discord.js");
   const db = require("../../lib/db");
   const boost = require("../../lib/boost");
   const config = require("../../../config.json");
@@ -11,7 +11,6 @@ module.exports = async(interaction)=>{
       const members = await interaction.guild.members.fetch();
 
       const bot = members.filter(m=>m.user.bot);
-
       const online = members.filter(member=>member.presence?.status === "online");
       const dnd = members.filter(member=>member.presence?.status === "dnd");
       const idle = members.filter(member=>member.presence?.status === "idle");
@@ -22,7 +21,7 @@ module.exports = async(interaction)=>{
       const desktop = members.filter(member=>member.presence?.clientStatus?.desktop);
 
       const channels = await interaction.guild.channels.fetch();
-
+      
       const text = channels.filter(ch=>ch.type === ChannelType.GuildText);
       const voice = channels.filter(ch=>ch.type === ChannelType.GuildVoice);
       const category = channels.filter(ch=>ch.type === ChannelType.GuildCategory);
@@ -31,7 +30,7 @@ module.exports = async(interaction)=>{
       const emojis = await interaction.guild.emojis.fetch();
       const stickers = await interaction.guild.stickers.fetch();
 
-      const shardCount = interaction.client.shard ? `\nシャード:${interaction.client.shard.shardIdForGuildId(interaction.guild.id,config.shards)}番`:""
+      const shardCount = interaction.client.shard ? `\nシャード:${ShardClientUtil.shardIdForGuildId(interaction.guild.id,config.shards)}番`:""
 
       const stats = await db(`SELECT * FROM stats WHERE id = ${interaction.guild.id};`);
 
