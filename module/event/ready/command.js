6435d360beb1fc8392a10bc0e7d3381c6730df4e
Taka005 <01taka005@gmail.com>
2023-12-31 10:48:24 +0900

    test:1

--- module/event/ready/command.js
+++ module/event/ready/command.js
@@ -1,13 +1,10 @@
 module.exports = async(client)=>{
   require("dotenv").config();
-  const { REST, Routes } = require("discord.js");
+  const { Routes } = require("discord.js");
   const log = require("../../lib/log");
   const commands = require("../../../file/commandlist");
 
-  const rest = new REST({version:"10"})
-    .setToken(process.env.BOT_TOKEN);
-
-  await rest.put(Routes.applicationCommands(client.application.id),{
+  await client.rest.put(Routes.applicationCommands(client.application.id),{
     body: Object.values(commands).map(command=>command.data)
   });
 
