565696c4c57efde1b8d878cbf235fa47832a7e3e
Taka005 <01taka005@gmail.com>
2024-02-14 09:15:18 +0900

    fix:誤字修正

--- module/lib/ignore.js
+++ module/lib/ignore.js
@@ -5,7 +5,7 @@ module.exports = {
     if(type){
       await db(`INSERT INTO \`ignore\` (id, ${type}, time) VALUES("${guildId}",true,NOW()) ON DUPLICATE KEY UPDATE id = VALUES (id), ${type} = VALUES(${type}), time = VALUES (time);`);
     }else{
-      await db(`INSERT INTO \`ignore\` (id, bump, dissoku, up, expend, time) VALUES("${guildId}",true,true,true,true,NOW()) ON DUPLICATE KEY UPDATE id = VALUES (id), bump = VALUES(bump), dissoku = VALUES(dissoku), up = VALUES(up), expend = VALUES(expend), time = VALUES (time);`);
+      await db(`INSERT INTO \`ignore\` (id, bump, dissoku, up, expand, time) VALUES("${guildId}",true,true,true,true,NOW()) ON DUPLICATE KEY UPDATE id = VALUES (id), bump = VALUES(bump), dissoku = VALUES(dissoku), up = VALUES(up), expand = VALUES(expand), time = VALUES (time);`);
     }
   },
   disable:async(guildId,type)=>{
