3113a65a868bddeecaececbb27adfd8d5760b12e
Taka005 <01taka005@gmail.com>
2024-04-07 22:06:23 +0900

    change:購入処理を統一

---
+++ file/products.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: "yellow",
+    name: "GC黄色",
+    price: 10
+  },
+  {
+    id: "red",
+    name: "GC赤色",
+    price: 100
+  },
+  {
+    id: "blue",
+    name: "GC青色",
+    price: 800
+  }
+]
