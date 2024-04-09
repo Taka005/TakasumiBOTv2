1cb45be0855a193844098617229fbc5d7d6937da
Taka005 <01taka005@gmail.com>
2024-04-09 17:34:43 +0900

    fix: ミス

--- module/event/messageCreate/gc.js
+++ module/event/messageCreate/gc.js
@@ -54,7 +54,11 @@ module.exports = async(message)=>{
 
   let color = Colors.Green;
   const data = await money.get(message.author.id);
-  if(data?.blue > 0){
+
+  if(data?.random > 0){
+    color = Math.floor(Math.random()*(0xffffff + 1));
+    await db(`UPDATE money SET random = ${data.random-1} WHERE id = ${message.author.id}`);
+  }else if(data?.blue > 0){
     color = Colors.Blue;
     await db(`UPDATE money SET blue = ${data.blue-1} WHERE id = ${message.author.id}`);
   }else if(data?.red > 0){
@@ -63,9 +67,6 @@ module.exports = async(message)=>{
   }else if(data?.yellow > 0){
     color = Colors.Yellow;
     await db(`UPDATE money SET yellow = ${data.yellow-1} WHERE id = ${message.author.id}`);
-  }else if(data?.random > 0){
-    color = Math.floor(Math.random()*(0xffffff + 1));
-    await db(`UPDATE money SET random = ${data.random-1} WHERE id = ${message.author.id}`);
   }
 
   const embed = [{
