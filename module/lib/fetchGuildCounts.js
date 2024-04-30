a34f2017ed82f613734936b96aa56e9d54f5be2e
Taka005 <01taka005@gmail.com>
2024-04-30 16:58:39 +0900

    change:表示変更

--- module/lib/fetchGuildCounts.js
+++ module/lib/fetchGuildCounts.js
@@ -1,8 +1,8 @@
 module.exports = async(client)=>{
   if(client.shard){
-    return (await client.shard.broadcastEval(c=>c.guilds.cache.size))
+    return (await client.shard.broadcastEval(c=>c.guilds.cache.filter(g=>g.available).size))
       .reduce((a,c)=>a+c,0);
   }else{
-    return client.guilds.cache.size;
+    return client.guilds.cache.filter(g=>g.available).size;
   }
 }
