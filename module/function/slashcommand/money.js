2a8efcc9482dc7a3126ab49d3757146765b410dd
Taka005 <01taka005@gmail.com>
2024-05-09 23:05:21 +0900

    add:history

--- module/function/slashcommand/money.js
+++ module/function/slashcommand/money.js
@@ -1,17 +1,18 @@
 module.exports = async(interaction)=>{
-  const { Colors } = require("discord.js");
+  const { AttachmentBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors } = require("discord.js");
+  const fetch = require("node-fetch");
   const money = require("../../lib/money");
   const db = require("../../lib/db");
   const products = require("../../../file/products");
+  const config = require("../../../config.json");
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "money"){
-    const user = interaction.options.getUser("user");
 
-    const rank = (await db("SELECT * FROM money;"))
-      .sort((m1,m2)=>m2.amount - m1.amount)
-      .map(m=>m.id);
+    if(interaction.options.getSubcommand() === "info"){
+      const rank = (await db("SELECT * FROM money;"))
+        .sort((m1,m2)=>m2.amount - m1.amount)
+        .map(m=>m.id);
 
-    if(!user){
       const data = await money.get(interaction.user.id);
       await interaction.reply({
         embeds:[{
@@ -36,31 +37,79 @@ module.exports = async(interaction)=>{
           ]
         }]
       });
-    }else{
-      const data = await money.get(user.id);
-      await interaction.reply({
-        embeds:[{
-          color: Colors.Green,
-          author:{
-            name: `${user.tag}の持ち物`,
-            icon_url: "https://cdn.takasumibot.com/images/system/success.png"
+    }else if(interaction.options.getSubcommand() === "log"){
+
+      await interaction.deferReply();
+      try{
+        const history = (await db(`SELECT * FROM history WHERE user = ${interaction.user.id}`))
+          .map(his=>([his.id,his.reason,his.amount,new Date(his.time).toLocaleString()]));
+
+        if(!history[0]) return await interaction.reply({
+          embeds:[{
+            color: Colors.Red,
+            author:{
+              name: "表示できませんでした",
+              icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+            },
+            description: "取引履歴が存在しません"
+          }],
+          ephemeral: true
+        });
+
+        const data = await fetch(`${config.api.graph}/table`,{
+          "method": "POST",
+          "headers":{
+            "Content-Type": "application/json"
           },
-          fields:[
-            {
-              name: "残高",
-              value: `${data.amount||0}コイン`
+          "body": JSON.stringify({
+            "label": ["取引ID","内容","金額","取引日時"],
+            "data": history
+          })
+        }).then(res=>res.blob());
+
+        await interaction.editReply({
+          embeds:[{
+            color: Colors.Green,
+            author:{
+              name: "取引履歴",
+              icon_url: "https://cdn.takasumibot.com/images/system/success.png"
             },
-            {
-              name: "アイテム",
-              value: `${products.map(pro=>`${pro.name}: ${data[pro.id]||"0"}`).join("\n")}\n株: ${data.stock||"0"}個`,
-            },
-            {
-              name: "順位",
-              value: `${data ? `${rank.indexOf(user.id)+1}/${rank.length}位`:"不明"}`
+            description: "過去3日分を表示しています",
+            image:{
+              url: "attachment://history.png"
             }
+          }],
+          files:[
+            new AttachmentBuilder()
+              .setFile(data.stream())
+              .setName("history.png")
           ]
-        }]
-      });
+        });
+      }catch(error){
+        await interaction.editReply({
+          embeds:[{
+            color: Colors.Red,
+            author:{
+              name: "表示できませんでした",
+              icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+            },
+            fields:[
+              {
+                name: "エラーコード",
+                value: `\`\`\`${error}\`\`\``
+              }
+            ]
+          }],
+          components:[
+            new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setLabel("サポートサーバー")
+                  .setURL(config.inviteUrl)
+                  .setStyle(ButtonStyle.Link))
+          ]
+        });
+      }
     }
   }
 }
