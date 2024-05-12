90c24534a0609cdbc477f73a22c90cb0978d51b9
Taka005 <01taka005@gmail.com>
2024-05-12 10:49:34 +0900

    style:調整

--- module/lib/time.js
+++ module/lib/time.js
@@ -1,8 +1,8 @@
-module.exports = (ms)=>{  
-  const t = Math.round(ms / 1000);
-  const h = Math.floor(t / 3600);
-  const m = Math.floor((t - h * 3600) / 60);
-  const s = Math.floor(t - h * 3600 - m * 60);
+module.exports = (ms)=>{
+  const t = Math.round(ms/1000);
+  const h = Math.floor(t/3600);
+  const m = Math.floor((t - h*3600)/60);
+  const s = Math.floor(t - h*3600 - m*60);
 
   if(h === 0){
     if(m === 0){
