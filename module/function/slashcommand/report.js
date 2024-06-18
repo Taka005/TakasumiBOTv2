3aa48ca95e79cf90bc7e933ef1cb574bf87f41fb
Taka005 <01taka005@gmail.com>
2024-06-18 20:10:42 +0900

    fix:調整

--- module/function/slashcommand/report.js
+++ module/function/slashcommand/report.js
@@ -1,22 +1,7 @@
-const Spam = require("../../lib/spam");
-const spam = new Spam(300000,true);
-
 module.exports = async(interaction)=>{
   const { ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, Colors } = require("discord.js");
   if(!interaction.isChatInputCommand()) return;
   if(interaction.commandName === "report"){
-
-    if(spam.count(interaction.user.id)) return await interaction.reply({
-      embeds:[{
-        color: Colors.Red,
-        author:{
-          name: "通報できませんでした",
-          icon_url: "https://cdn.takasumibot.com/images/system/error.png"
-        },
-        description: "連続して通報することはできません\nしばらく待ってください"
-      }],
-      ephemeral: true
-    });
 
     const report = new ModalBuilder()
       .setCustomId("report")
