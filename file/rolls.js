01f87d97ee2d4f77aed6d5e633f7539235277063
Taka005 <01taka005@gmail.com>
2024-07-22 16:42:46 +0900

    change:rolls追加

--- file/rolls.js
+++ file/rolls.js
@@ -2,5 +2,25 @@ module.exports = [
   {
     price: 1000,
     rate: 0.5
+  },
+  {
+    price: 12000,
+    rate: 0.25
+  },
+  {
+    price: 20000,
+    rate: 0.12
+  },
+  {
+    price: 30000,
+    rate: 0.08
+  },
+  {
+    price: 50000,
+    rate: 0.04
+  },
+  {
+    price: 60000,
+    rate: 0.01
   }
 ]
