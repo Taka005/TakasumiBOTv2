debc4afef31fb6024f4affe4397a31a85b92c4ac
Taka005 <01taka005@gmail.com>
2024-10-14 20:02:56 +0900

    add:商品システム追加

---
+++ module/function/product/list.js
@@ -0,0 +1,66 @@
+module.exports = async(interaction)=>{
+  const { Colors } = require("discord.js");
+  const db = require("../../lib/db");
+  if(interaction.options.getSubcommand() === "list"){
+    const type = interaction.options.getString("type");
+
+    if(type === "all"){
+      const data = await db(`SELECT * FROM product ORDER BY RAND() LIMIT 25;`);
+      if(!data[0]) return await interaction.reply({
+        embeds:[{
+          color: Colors.Red,
+          author:{
+            name: "商品を表示できませんでした",
+            icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+          },
+          description: "購入できる商品が存在しません"
+        }],
+        ephemeral: true
+      });
+
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Green,
+          author:{
+            name: "商品一覧",
+            icon_url: "https://cdn.takasumibot.com/images/system/success.png"
+          },
+          description: "ランダムな商品が表示されています",
+          fields: data.map(pro=>({
+            name: `${pro.name} ${pro.price}コイン`,
+            value: `商品ID: ${pro.id}`
+          }))
+        }],
+        ephemeral: true
+      });
+    }else{
+      const data = await db(`SELECT * FROM product WHERE seller = ${interaction.user.id};`);
+      if(!data[0]) return await interaction.reply({
+        embeds:[{
+          color: Colors.Red,
+          author:{
+            name: "作成した商品を表示できませんでした",
+            icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+          },
+          description: "商品をまだ作成していません"
+        }],
+        ephemeral: true
+      });
+
+      await interaction.reply({
+        embeds:[{
+          color: Colors.Green,
+          author:{
+            name: "作成した商品一覧",
+            icon_url: "https://cdn.takasumibot.com/images/system/success.png"
+          },
+          fields: data.map(pro=>({
+            name: `${pro.name} ${pro.price}コイン`,
+            value: pro.content.lenght > 10 ? `${pro.content.slice(0,9)}...` : pro.content
+          }))
+        }],
+        ephemeral: true
+      });
+    }
+  }
+}
---
+++ module/function/product/pay.js
@@ -0,0 +1,50 @@
+module.exports = async(interaction)=>{
+  const { Colors } = require("discord.js");
+  const db = require("../../lib/db");
+  const money = require("../../lib/money");
+  if(interaction.options.getSubcommand() === "pay"){
+    const id = interaction.options.getString("id");
+
+    const product = await db(`SELECT * FROM product WHERE id = ${id};`);
+    if(!product[0]) return await interaction.reply({
+      embeds:[{
+        color: Colors.Red,
+        author:{
+          name: "購入できませんでした",
+          icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+        },
+        description: "存在しない商品です"
+      }],
+      ephemeral: true
+    });
+
+    const data = await money.get(interaction.user.id);
+
+    if(data.amount<product.price) return await interaction.reply({
+      embeds:[{
+        color: Colors.Red,
+        author:{
+          name: "購入できませんでした",
+          icon_url: "https://cdn.takasumibot.com/images/system/error.png"
+        },
+        description: "所持金が不足しています"
+      }],
+      ephemeral: true
+    });
+
+    await money.add(product.seller,product.price,`${product.name}の売り上げ`);
+    await money.delete(interaction.user.id,product.price,`${product.name}の購入`);
+    await db(`DELETE FROM product WHERE id = ${product.id};`);
+    await interaction.reply({
+      embeds:[{
+        color: Colors.Green,
+        author:{
+          name: `${product.name}(${product.price}コイン)を購入しました`,
+          icon_url: "https://cdn.takasumibot.com/images/system/success.png"
+        },
+        description: product.content
+      }],
+      ephemeral: true
+    });
+  }
+}
