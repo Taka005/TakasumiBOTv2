d8d7835120829ea25d9b76e6eaf7c73ffb341839
Taka005 <01taka005@gmail.com>
2024-10-18 22:32:31 +0900

    change:戻す

--- module/function/product/list.js
+++ module/function/product/list.js
@@ -4,10 +4,9 @@ module.exports = async(interaction)=>{
   const fetchUser = require("../../lib/fetchUser");
   if(interaction.options.getSubcommand() === "list"){
     const type = interaction.options.getString("type");
-    const page = interaction.options.getInteger("page");
 
     if(type === "all"){
-      const data = await db(`SELECT * FROM product ORDER BY id LIMIT 15 OFFSET (${page} - 1)*15;`);
+      const data = await db(`SELECT * FROM product ORDER BY RAND() LIMIT 25;`);
       if(!data[0]) return await interaction.reply({
         embeds:[{
           color: Colors.Red,
@@ -36,6 +35,7 @@ module.exports = async(interaction)=>{
             name: "商品一覧",
             icon_url: "https://cdn.takasumibot.com/images/system/success.png"
           },
+          description: "ランダムな商品が表示されています",
           fields: list
         }]
       });
